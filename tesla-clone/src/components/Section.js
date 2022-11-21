93756124e9f57d87fae1f233246f52b2357448b9
TruongMinh27 <minhnct0319@dhv.edu.vn>
2022-11-21 11:47:53 +0700

    start tesla clone --add section

---
+++ tesla-clone/src/components/Section.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Section({ title,decription,image,leftButton,rightButton }) {
+  return (
+    <Wrap bgImage={image}>
+        <ItemText>
+            <h1>{title}</h1>
+              <p>{decription}</p>
+        </ItemText>
+        <Button>
+            <ButtonGroup>
+                <LeftButton>
+                      {leftButton}
+                </LeftButton>
+                  {rightButton &&
+                   <RightButton>
+                      {rightButton}
+                  </RightButton>}
+            </ButtonGroup>
+            <DownArrow src="./images/down-arrow.svg" />
+        </Button>
+    </Wrap>
+  )
+}
+
+export default Section
+
+const Wrap = styled.div`
+    width:100vw;
+    height:100vh;
+    background-size:cover;
+    background-position: center;
+    background-repeat:no-repeat;
+    display:flex;
+    flex-direction:column;
+    justify-content:space-between;
+    align-items:center;
+    background-image: ${props => `url("./images/${props.bgImage}")`};
+
+`
+
+const ItemText = styled.div`
+    padding-top:15vh;
+    text-align:center;
+
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom:30px;
+    @media (max-width:768px){
+        flex-direction:column;
+    }
+`
+
+const LeftButton = styled.div`
+    background-color: rgb(23,26,32,0.8);
+    color: #fff;
+    width:256px;
+    height:40px;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    border-radius:100px;
+    text-align:center;
+    font-size:12px;
+    text-transform:uppercase;
+    cursor:pointer;
+    opacity:0.9;
+    margin:5px;
+
+`
+const RightButton = styled(LeftButton)`
+    color :#000;
+    background-color:rgba(244, 244, 244, 0.65);
+`
+
+const DownArrow = styled.img`
+    margin-top:20px;
+    overflow-x:hidden;
+    height:40px;
+    animation:animateDown infinite 1.5s;
+`
+
+const Button = styled.div`
+
+`
